3e17146e7f1824c1e4045ca0a1778d3a8f4c40f7
hebertmfrancato <hebertmfrancato@gmail.com>
2020-06-06 05:28:52 -0300

    Alterações

--- src/server.js
+++ src/server.js
@@ -1,5 +1,31 @@
 const express = require("express")
 const server = express()
 
-// liagr o servidor
+// Configurar pasta pública
+server.use(express.static("public"))
+
+// Utilizando templante engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// configurar caminhos da minha aplicação
+// página inicial
+// req: Reaquisição
+// res: Resposta
+server.get("/", (req, res)=> {
+    return res.render("index.html", { title: "Um título"})
+})
+
+server.get("/create-point", (req, res)=> {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res)=> {
+    return res.render("search-results.html")
+})
+
+// ligar o servidor
 server.listen(3000)
